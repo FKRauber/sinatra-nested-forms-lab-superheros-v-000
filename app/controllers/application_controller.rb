9f05b65e7ea3467f6ae550dc3a7d611e89dd623c
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-08-15 21:44:41 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,9 @@ class App < Sinatra::Base
     post '/teams' do
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
       members = params[:team][:members]
+      @super_heroes = members.map do |member|
+        SuperHero.new({name: member[:name], power: member[:power], biography: member[:biography]})
+      end
       erb :team
     end
 end
