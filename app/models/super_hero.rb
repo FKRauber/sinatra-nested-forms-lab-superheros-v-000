134b3a58034f87866747d37c04bc430c32a724a6
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-08-15 22:03:10 +0000

    automatically backed up by learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,15 +1,15 @@
 class SuperHero
   attr_accessor :name, :power, :bio
 
-  def initialize(args={})
-    @name = args[:name]
-    @power = args[:power]
-    @bio = args[:bio]
-    self.save
-  end
-
   def self.all
     @@all ||= []
+  end
+
+  def initialize(opts={})
+    @name  = opts[:name]
+    @power = opts[:power]
+    @bio   = opts[:bio]
+    self.save
   end
 
   def save
