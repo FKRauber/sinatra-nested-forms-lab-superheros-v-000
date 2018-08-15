c0efc36432c7657cb29b4c042d39bf9f03bcb56e
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-08-15 21:54:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
       members = params[:team][:members]
       @super_heroes = members.map do |member|
-        SuperHero.new({name: member[:name], power: member[:power], biography: member[:biography]})
+        SuperHero.new({name: member[:name], power: member[:power], bio: member[:bio]})
       end
       erb :team
     end
