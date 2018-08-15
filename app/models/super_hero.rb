767b15366966858a3926dd9faf79795c8db12cff
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-08-15 21:47:36 +0000

    automatically backed up by learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,3 +1,9 @@
 class SuperHero
+  attr_accessor :name, :power, :biography
 
+  def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    @biography = args[:biography]
+  end
 end
